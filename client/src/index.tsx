a54af27eb4f0bdaa14b1ebe630045ba3568749ae
Ben Sahagun <benedicksahagun@gmail.com>
2021-07-06 00:22:08 +0800

    feat: enforce dark mode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ColorModeScript } from '@chakra-ui/react';
+import theme from './theme';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: process.env.REACT_APP_SERVER_URL,
   cache: new InMemoryCache({
     addTypename: false,
   }),
@@ -12,6 +14,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </ApolloProvider>,
   document.getElementById('root')
